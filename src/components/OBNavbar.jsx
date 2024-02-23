6b3fb11d1a27f56dc23c1e131a18759d3736c499
cthao17 <rororm17@gmail.com>
2024-02-23 08:52:03 -0600

    Update OBNavbar.jsx
    Add home page navigation when clicking on OB Icon

--- src/components/OBNavbar.jsx
+++ src/components/OBNavbar.jsx
@@ -3,10 +3,16 @@ import "./styles.css"
 function OBNavbar(props) {
     // logo png (should eventually host our images locally): 
     // https://www.myorangebutton.com/wp-content/uploads/2023/05/cropped-Orange-button-logo-512.png
+    
+    // Reloads ot home page when clicking on OB Icon
+    const reloadHome = () => {
+        window.location.reload();
+    };
+
     return (
         <Navbar expand="lg">
             <Container>
-                <Navbar.Brand >
+                <Navbar.Brand onClick={reloadHome}>
                         <img id = "logo" src="https://www.myorangebutton.com/wp-content/uploads/2023/05/cropped-Orange-button-logo-512.png" alt="ob logo" />
                     </Navbar.Brand>
                 <Navbar.Text id="navbar-text">
@@ -15,7 +21,6 @@ function OBNavbar(props) {
                     <Navbar.Toggle aria-controls="navbar-items"/>
                     <Navbar.Collapse id = "navbar-items">
                         <Nav className = "me-auto">
-                            
                             <Nav.Link href = "https://productregistry.myorangebutton.com/product/" id = "link" target="_blank">
                                 Product Registry
                                 </Nav.Link>
